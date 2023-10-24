1854bb7573a4e644c940f07fe66d7714be0278c2
TroyHacks <5659019+troyhacks@users.noreply.github.com>
2023-10-24 00:52:56 -0400

    debugging

--- pyrekordbox/anlz/file.py
+++ pyrekordbox/anlz/file.py
@@ -108,7 +108,7 @@ class AnlzFile(abc.Mapping):
                 tag_data = bytearray(data[i:])
                 secretcode = bytearray.fromhex("CB E1 EE FA E5 EE AD EE E9 D2 E9 EB E1 E9 F3 E8 E9 F4 E1")
                 len_entries = Int16ub.parse(tag_data[16:])
-                for i in range(len(tag_data[18:]):
+                for i in range(len(tag_data[18:])):
                     decrypt = secretcode[i%len(secretcode)]+len_entries
                     if decrypt > 255 :
                         decrypt -= 256
